0438e1788433b01238e92f6468ce58174911e82b
lkhagva-git <sukhochirlkhagva@gmail.com>
2024-12-06 11:31:59 +0800

    Сайжруулалт.

--- main/views.py
+++ main/views.py
@@ -1,16 +1,9 @@
 import logging
-import re
 import datetime
-import json
 
-from django.contrib.auth import logout
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import JsonResponse
 
 
 from rest_framework.decorators import api_view, permission_classes
